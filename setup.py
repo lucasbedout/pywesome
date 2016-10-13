a27c522fb618a652234520ad04e79dccd8573c90
lucasbedout <lucas.bedout@free.fr>
2016-10-13 16:47:33 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'pywesome',
+    version = '201610131645',
+    description = 'Python lists on steroidss',
+    packages = [
+        'pywesome.utils',
+    ]
+)
