b211460c1225fa2042c22387b329677749517ffe
lucasbedout <lucas.bedout@free.fr>
2016-10-27 11:30:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pywesome',
-    version = '201610131702',
+    version = '201610271130',
     description = 'Python lists on steroidss',
     packages = ['pywesome']
 )
