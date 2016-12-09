7a6d2f109fb59a32ccdbab00ee495a3cc1ab6770
Douglas <douglas@networklocum.com>
2016-12-09 15:29:02 +0000

    Unlist.

--- pywesome/collection.py
+++ pywesome/collection.py
@@ -1,9 +1,15 @@
-from functools import partial
-
 from . import pywesome
 
 
-class Pywesome(list):
+def collect(entities):
+    return Pywesome.collect(entities)
+
+
+class Pywesome(object):
+    entities = []
+
+    def __init__(self, entities):
+        self.entities = entities
 
     @staticmethod
     def collect(entities):
@@ -13,53 +19,52 @@ class Pywesome(list):
         '''
         return Pywesome(entities)
 
-    def __getattribute__(self, name, *args, **kwargs):
+    def __getattr__(self, name):
         '''
         This method override enables the class to include all the methods in the
         pywesome module (file) in the class.
         '''
-
-        if name == '__getstate__':
-            # This has to be explicitly raised in this method, not a nested
-            # method for pickling (copy) to work properly.
-            raise AttributeError()
         try:
-            return super().__getattribute__(name, *args, **kwargs)
-        except AttributeError as e:
+            return object.__getattr__(self, name)
+        except AttributeError:
             def forward(*args, **kwargs):
-                result = getattr(pywesome, name)(self, *args, **kwargs)
+                result = getattr(pywesome, name)(self.entities, *args, **kwargs)
                 if isinstance(result, list):
                     return self.collect(result)
                 return result
+
             return forward
 
     def __getitem__(self, *args, **kwargs):
-        result = super().__getitem__(*args, **kwargs)
+        result = self.entities.__getitem__(*args, **kwargs)
         try:
             return self.collect(result)
         except TypeError:
             return result
 
     def __reversed__(self, *args, **kwargs):
-        return self.collect(self.__reversed__(*args, **kwargs))
-
-    def __getslice__(self, *args, **kwargs):
-        return self.collect(self.__getslice__(self, *args, **kwargs))
+        return self.collect(self.entities.__reversed__(*args, **kwargs))
 
     def __add__(self, *args, **kwargs):
-        return self.collect(self.__add__(self, *args, **kwargs))
+        return self.collect(self.entities.__add__(self, *args, **kwargs))
 
     def __mul__(self, *args, **kwargs):
-        return self.collect(self.__mul__(self, *args, **kwargs))
+        return self.collect(self.entities.__mul__(self, *args, **kwargs))
 
     def count(self):
-        return len(self)
+        return len(self.entities)
+
+    def append(self, item):
+        self.entities.append(item)
 
     def prepend(self, item):
-        self.insert(0, item)
+        self.entities.insert(0, item)
+
+    def pop(self, *args):
+        return self.entities.pop(*args)
 
     def to_list(self):
-        return list(self)
+        return self.entities
 
     def to_json(self):
         return self.json()
