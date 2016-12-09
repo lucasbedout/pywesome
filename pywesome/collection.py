de8ca0634b2dca0be1579f8c549e06aa7229fbe2
Douglas <douglas@networklocum.com>
2016-12-09 12:41:02 +0000

    Listify collection.

--- pywesome/collection.py
+++ pywesome/collection.py
@@ -1,45 +1,66 @@
+from functools import partial
+
 from . import pywesome
 
-def collect(entities):
-    return Pywesome(entities)
 
-class Pywesome(object):
+class Pywesome(list):
 
-    entities = []
+    @staticmethod
+    def collect(entities):
+        '''
+        Re-implement this method if you extend this class. It is used to cast
+        list methods that return lists into the intended class.
+        '''
+        return Pywesome(entities)
 
-    def __init__(self, entities):
-        self.entities = entities 
+    def __getattribute__(self, name, *args, **kwargs):
+        '''
+        This method override enables the class to include all the methods in the
+        pywesome module (file) in the class.
+        '''
 
-    def __getattr__(self, name):
+        if name == '__getstate__':
+            # This has to be explicitly raised in this method, not a nested
+            # method for pickling (copy) to work properly.
+            raise AttributeError()
         try:
-            return object.__getattr__(self, name)
-        except AttributeError:
+            return super().__getattribute__(name, *args, **kwargs)
+        except AttributeError as e:
             def forward(*args, **kwargs):
-                result = getattr(pywesome, name)(self.entities, *args)
+                result = getattr(pywesome, name)(self, *args, **kwargs)
                 if isinstance(result, list):
-                    return collect(result)
+                    return self.collect(result)
                 return result
             return forward
 
-    def append(self, item):
-        self.entities.append(item)
+    def __getitem__(self, *args, **kwargs):
+        result = super().__getitem__(*args, **kwargs)
+        try:
+            return self.collect(result)
+        except TypeError:
+            return result
 
-    def pop(self, key=None):
-        if not key:
-            key = self.count() - 1
-        return self.entities.pop(key)
+    def __reversed__(self, *args, **kwargs):
+        return self.collect(self.__reversed__(*args, **kwargs))
 
-    def prepend(self, item):
-        self.entities.insert(0, item)
+    def __getslice__(self, *args, **kwargs):
+        return self.collect(self.__getslice__(self, *args, **kwargs))
+
+    def __add__(self, *args, **kwargs):
+        return self.collect(self.__add__(self, *args, **kwargs))
+
+    def __mul__(self, *args, **kwargs):
+        return self.collect(self.__mul__(self, *args, **kwargs))
 
     def count(self):
-        return len(self.entities)
+        return len(self)
+
+    def prepend(self, item):
+        self.insert(0, item)
 
     def to_list(self):
-        return self.entities
+        return list(self)
 
     def to_json(self):
         return self.json()
-
-
 
