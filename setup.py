7f5797602c3266ab5c1777d6a00c55c85e16397e
lucasbedout <lucas.bedout@free.fr>
2016-12-14 12:00:21 +0000

    correct release

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = 'pywesome',
-    version = '1.0',
+    version = '1.0.2',
     description = 'Python lists on steroidss',
     packages = ['pywesome'],
     author = 'Lucas Bedout',
 	author_email = 'roadtostark@gmail.com',
 	url = 'https://github.com/lucasbedout/pywesome', # use the URL to the github repo
-	download_url = 'https://github.com/lucasbedout/pywesome/tarball/v1.0', # I'll explain this in a second
+	download_url = 'https://github.com/lucasbedout/pywesome/tarball/v1.0.2', # I'll explain this in a second
 	keywords = ['list', 'collection', 'map', 'reduce', 'filter'],
 )
