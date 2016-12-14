ee6a2dcabb46c960103fb423b64fbfb47e26f38b
lucasbedout <lucas.bedout@free.fr>
2016-12-14 11:58:48 +0000

    go live

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author = 'Lucas Bedout',
 	author_email = 'roadtostark@gmail.com',
 	url = 'https://github.com/lucasbedout/pywesome', # use the URL to the github repo
-	download_url = 'https://github.com/lucasbedout/pywesome/tarball/1.0', # I'll explain this in a second
+	download_url = 'https://github.com/lucasbedout/pywesome/tarball/v1.0', # I'll explain this in a second
 	keywords = ['list', 'collection', 'map', 'reduce', 'filter'],
 )
